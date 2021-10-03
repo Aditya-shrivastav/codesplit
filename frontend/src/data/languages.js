e6d667b9b160626c747a9d8a7aba4904e4eb681a
hariom-ojha23 <hari_om.scsebtech@galgotiasuniversity.edu.in>
2021-10-03 08:11:10 +0530

    created join modal and create modal in home screen and added languages array in data folder

---
+++ frontend/src/data/languages.js
@@ -0,0 +1,13 @@
+export const languages = [
+  'C++',
+  'Java',
+  'Python',
+  'JavaScript',
+  'HTML5',
+  'CSS3',
+  'JSON',
+  'Rust',
+  'SQL',
+  'XML',
+  'Markdown',
+]
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Container, Row, Col, Button, Image } from 'react-bootstrap'
+import React, { useState } from 'react'
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Image,
+  Form,
+  Modal,
+} from 'react-bootstrap'
+import { languages } from '../data/languages'
 
 const HomeScreen = () => {
+  const [modalShow, setModalShow] = useState(false)
+  const [type, setType] = useState('create')
+
+  const handleCreate = () => {
+    setType('create')
+    setModalShow(true)
+  }
+
+  const handleJoin = () => {
+    setType('join')
+    setModalShow(true)
+  }
+
   return (
     <>
       <Container>
         <Row style={{ minHeight: '80vh' }} className='p-3 align-items-center'>
-          <Col xs={12} md={6} className='px-md-5 px-3 my-auto'>
+          <Col xs={12} md={6} className='px-lg-5 px-3 my-auto'>
             <h1 className='homeScreen-title'>CodeSplit!</h1>
             <h1 className='homeScreen-title'>Collaborate in Real Time</h1>
             <Row className='mt-5 btn-row'>
@@ -14,6 +36,7 @@ const HomeScreen = () => {
                 className='btn homeScreen-btn my-1 mx-1'
                 type='button'
                 variant='primary'
+                onClick={handleCreate}
               >
                 Create
               </Button>
@@ -21,15 +44,72 @@ const HomeScreen = () => {
                 type='button'
                 className='btn homeScreen-btn my-1 mx-1'
                 variant='dark'
+                onClick={handleJoin}
               >
                 Join
               </Button>
             </Row>
           </Col>
-          <Col xs={12} md={6} className='px-md-5'>
+          <Col xs={12} md={6} className='px-lg-5'>
             <Image src='/images/mac.png' fluid />
           </Col>
         </Row>
+
+        {type === 'create' ? (
+          <Modal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            size='md'
+            centered
+          >
+            <Modal.Body>
+              <h2 className='text-center p-3'>Choose A Language</h2>
+              <Form.Control as='select' className='my-3'>
+                {languages.slice(0, 3).map((x) => (
+                  <option className='lang-options' key={x} val={x}>
+                    {x}
+                  </option>
+                ))}
+              </Form.Control>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className='btn' onClick={handleCreate}>
+                Create
+              </Button>
+              <Button
+                variant='secondary'
+                className='btn'
+                onClick={() => setModalShow(false)}
+              >
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        ) : (
+          <Modal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            size='md'
+            centered
+          >
+            <Modal.Body>
+              <h2 className='text-center p-3'>Enter Room Id</h2>
+              <Form.Control placeholder='Enter Room Id' className='my-3' />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className='btn' onClick={handleJoin}>
+                Join
+              </Button>
+              <Button
+                variant='secondary'
+                className='btn'
+                onClick={() => setModalShow(false)}
+              >
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
       </Container>
     </>
   )
